03befe2af39165308de0f6c83995e88fa3c5dacf
Christian Schou <christian.schou@hotmail.com>
2023-04-25 10:52:03 +0200

    Added new settings classes
    New settings classes has been added to support the new configuration file.

--- src/Infrastructure/Settings/EventBusSettings.cs
+++ src/Infrastructure/Settings/EventBusSettings.cs
@@ -5,6 +5,22 @@
     /// </summary>
     internal class EventBusSettings
     {
+        public string EventbusProvider { get; set; } = "rabbitmq";
+        public AzureServiceBusSettings? AzureServiceBusSettings { get; set; } = new AzureServiceBusSettings();
+        public RabbitmqSettings? RabbitmqSettings { get; set;} = new RabbitmqSettings();
+    }
+
+    internal class AzureServiceBusSettings
+    {
+        /// <summary>
+        /// Primary Connection String for Azure Service Bus.<br />
+        /// This can be achieved from the service bus IAM in the Azure Portal.
+        /// </summary>
+        public string ConnectionString { get; set; } 
+    }
+
+    internal class RabbitmqSettings
+    {
         /// <summary>
         /// Event Bus Host Address. By default set to "localhost".
         /// </summary>
